09917276f9e7787ac465696a0d90787b793b2f6e
jameshsu1125 <jameshsu1125@gmail.com>
2023-11-29 23:05:03 +0800

    Update config.ts

--- src/settings/config.ts
+++ src/settings/config.ts
@@ -12,7 +12,7 @@ export const REST_PATH = {
   drop: 'drop',
 };
 
-// export const TEASER_DATE = new Date(2023, 10, 30, 9, 0, 0);
-export const TEASER_DATE = new Date(2023, 10, 24, 9, 0, 0);
+export const TEASER_DATE = new Date(2023, 10, 30, 9, 0, 0);
+// export const TEASER_DATE = new Date(2023, 10, 24, 9, 0, 0);
 
 export const OPEN_DELAY = 2000;
